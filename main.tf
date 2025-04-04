ff8a835194de076de398d498d4bdd3991cd4c95f
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-04-04 12:51:04 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = us-east-1
+}
+
+resource "aws_s3_bucket" "s3-bucket-1" {
+  bucket = "s3-bucket-1"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_object" "sample_file" {
+  bucket = aws_s3_bucket.s3-bucket-1.bucket
+  key    = "bucket.txt"
+  source = "bucket.txt"
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.s3-bucket-1.bucket
+}
